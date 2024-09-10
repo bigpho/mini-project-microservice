68710314a82e62a4fcd5c4d20c0ede3a853bf613
BIGPHO <ferbianloekman@gmail.com>
2024-09-11 00:00:36 +0700

    Menambahkan actuator untuk count transaksi berhasil

--- src/main/java/id/bca/co/intra/mini_project/service/TransactionService.java
+++ src/main/java/id/bca/co/intra/mini_project/service/TransactionService.java
@@ -2,6 +2,8 @@ package id.bca.co.intra.mini_project.service;
 
 import id.bca.co.intra.mini_project.entity.Transaction;
 import id.bca.co.intra.mini_project.repository.TransactionRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,19 @@ public class TransactionService {
 
     @Autowired
     private TransactionRepository transactionRepository;
+    
+    private final Counter transactionCounter;
+
+    @Autowired
+    public TransactionService(MeterRegistry meterRegistry) {
+        // Mendaftarkan custom metric untuk menghitung transaksi
+        this.transactionCounter = meterRegistry.counter("transaction.count");
+    }
 
     public Transaction save(Transaction transaction){
-        return transactionRepository.save(transaction);
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        transactionCounter.increment();
+        return savedTransaction;
     }
 
     public List<Transaction> findAll(){
